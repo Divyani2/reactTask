385eedea0ceaa40a1cc5d8e6898213906dd623a8
Divyani <rathoredivyani210@gmail.com>
2022-12-29 11:36:13 +0530

    updated

---
+++ src/App.js
@@ -0,0 +1,45 @@
+import React from 'react';
+import './App.css';
+import FormTak from './Form.jsx/FormTask';
+// import Context from './component/Context';
+// import { ContextProvider } from './component/CreateContext';
+// import HOC from './component/HOC';
+// import HOC2 from './component/HOC2';
+// import FuncComp from './component/FuncComp'
+// import ClsComp from './component/ClsComp';
+// import UseProp from './component/UseProp';
+// import UseCls from './component/UseCls';
+// import Conditional from './component/Conditional';
+// import List from './component/List';
+// import Forms from './component/Forms';
+//import UncontrolledForm from './component/UncontrolledForm';
+// import StateUp from './component/StateUp';
+//import Form from './TODOLIST/Form';
+
+
+function App() {
+  return (
+    <div className='app'>
+      {/* <FuncComp /> */}
+      {/* <ClsComp name="diya" />
+      <UseProp name="div"/>
+      <UseProp name="divya" lastName="rathore"/>
+      <UseCls/>
+      <Conditional/>
+      <List /> */}
+      {/* <Forms />  */}
+       {/* <UncontrolledForm /> */}
+      {/*<StateUp /> 
+      */}
+      {/* <Form /> */}
+      {/* <ContextProvider value="Rathore" >
+      <Context/>
+      </ContextProvider>
+      <HOC />
+      <HOC2 /> */}
+      <FormTak/>
+    </div>
+  )
+}
+
+export default App;
